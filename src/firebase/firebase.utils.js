2aa03cf736133ab49d2233a226ed348e55bb3d82
Madhumitha Gautam <madhuvivaan@Madhumithas-MacBook-Pro.local>
2020-10-24 20:56:16 +0530

    Fixed Env Variables

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyCAxy7hVd8zAFwz7XJ4G86uRXAv0n64YEI",
-  authDomain: "crwndb-64c62.firebaseapp.com",
-  databaseURL: "https://crwndb-64c62.firebaseio.com",
-  projectId: "crwndb-64c62",
-  storageBucket: "crwndb-64c62.appspot.com",
-  messagingSenderId: "488250060976",
-  appId: "1:488250060976:web:c10ba1c9b88df6fce0cb75",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-RXXYQPVDGX"
 };
 
